33b558a25f58d045150444e1a84433214f3afec1
NewtonKamau <kamaunewton78@gmail.com>
2024-02-29 16:40:52 +0300

    initialized model with a simple test for the config

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-
+require("dotenv").config();
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API);
 
 // ...
 
-const model = genAI.getGenerativeModel({ model: "MODEL_NAME"});
 
-// ...
+async function run () {
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const prompt = "write a story about a magic backpack"
+    const result = await model.generateContent(prompt);
+    const response = await result.response  ;
+    console.log(response);
+    const text = await response.text();
+    console.log(text);
+    
+}
+run();
